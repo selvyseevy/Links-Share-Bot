ccff75288a88a82a593a5c1c557e30d494b0b716
selvyseevy <selvyseevy@gmail.com>
2026-05-07 06:37:38 +0530

    Modify bot token, app ID, and database URI
    Updated environment variables for bot configuration and database connection.

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 # Recommended
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8707816870:AAESQbyDBhaEj6x3Jyd4t3GxaEMf1h_TJYA")
+APP_ID = int(os.environ.get("APP_ID", "8707816870"))
+API_HASH = os.environ.get("API_HASH", "2a8d13c1d6ce82476411f003b6922e47")
 
 # Main
-OWNER_ID = int(os.environ.get("OWNER_ID", "6497757690"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8480922250"))
 PORT = os.environ.get("PORT", "8080")
 
 # Database
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "link")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://akdanimes_97:akdanimes_97@cluster0.5e3ojhb.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 #Auto approve 
 CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '').split()] # dont change anything 
